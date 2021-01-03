0afc838e6c174acc87e659c9123a8d1a1f75b2ca
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2021-01-03 19:14:05 +0530

    added location services

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_driver_app/Colors.dart';
 import 'package:uber_driver_app/tabs/earningsTab.dart';
 import 'package:uber_driver_app/tabs/homeTab.dart';
@@ -18,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   TabController tabController;
   int _selectedIndex = 0;
+  Position currentPosition;
 
   void onItemClicked(int index) {
     setState(() {
--- lib/tabs/homeTab.dart
+++ lib/tabs/homeTab.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_driver_app/globals.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -13,6 +14,34 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   GoogleMapController _mapController;
   Completer<GoogleMapController> _controller = Completer();
+  Position currentPosition;
+
+  void getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      print("Location Services disabled");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      print("Location services denied forever");
+      return;
+    }
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        print("denied");
+        return;
+      }
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPosition = position;
+    LatLng pos = LatLng(position.latitude, position.longitude);
+    _mapController.animateCamera(CameraUpdate.newLatLng(pos));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +50,11 @@ class _HomeTabState extends State<HomeTab> {
         GoogleMap(
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
-          mapType: MapType.normal,
+          mapType: MapType.terrain,
           onMapCreated: (GoogleMapController controller) {
             _controller.complete();
             _mapController = controller;
+            getCurrentLocation();
           },
           initialCameraPosition: googlePlex,
         ),
