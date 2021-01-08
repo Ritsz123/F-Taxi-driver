df098628f7dbdaf393e6764eeed72205b9742ba9
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2021-01-08 16:53:00 +0530

    added globals

---
+++ lib/globals.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+User currentFirebaseUser;
+
+final CameraPosition googlePlex = CameraPosition(
+  bearing: 192.8334901395799,
+  target: LatLng(37.43296265331129, -122.08832357078792),
+  tilt: 59.440717697143555,
+  zoom: 19.151926040649414,
+);
+
+StreamSubscription<Position> homeTabPositionStream;
