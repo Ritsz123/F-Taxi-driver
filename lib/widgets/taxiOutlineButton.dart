dc1ce24abb1fd356f45db5a0b014746d8bf5c989
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2021-01-17 10:42:40 +0530

    added custom outline buttons

---
+++ lib/widgets/taxiOutlineButton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:uber_driver_app/Colors.dart';
+
+class TaxiOutlineButton extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+  final Color color;
+
+  TaxiOutlineButton({this.title, this.onPressed, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      borderSide: BorderSide(color: color),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+      onPressed: onPressed,
+      color: color,
+      textColor: color,
+      child: Container(
+        height: 50.0,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 15.0,
+              fontFamily: 'Brand-Bold',
+              color: MyColors.colorText,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
