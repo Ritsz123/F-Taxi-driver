f2020f61a628855c471d82f2504f5e8bd1e8be7e
ritesh <riteshkhadse12@gmail.com>
2021-07-23 21:59:33 +0530

    add request_helper

---
+++ lib/helper/request_helper.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../globals.dart';
+
+class RequestHelper {
+  static final List _successResponseCodes = [200, 201, 202, 204, 206];
+
+  static Future<Map<String, dynamic>> getRequest({required String url, bool withAuthToken = false, String? token}) async {
+    try {
+      if(withAuthToken && token == null){
+        throw Exception('Token not provided for withAuthToken request');
+      }
+
+      http.Response response = await http.get(
+          Uri.parse(url),
+          headers: {
+            'authorization': 'Bearer $token'
+          }
+      );
+
+      if (_successResponseCodes.contains(response.statusCode)) {
+        // success
+        String data = response.body;
+        var decodedData = jsonDecode(data);
+        return decodedData as Map<String, dynamic>;
+      } else {
+        throw Exception('request failed ${response.statusCode} ${response.body}');
+      }
+    } catch (e) {
+      logger.e(e);
+      throw Exception(e);
+    }
+  }
+
+  static Future<Map<String, dynamic>> postRequest({required String url, required Map<String, dynamic> body, bool withAuthToken = false, String? token}) async {
+    try {
+      if(withAuthToken && token == null){
+        throw Exception('Token not provided for withAuthToken request');
+      }
+
+      http.Response response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': 'Bearer $token'
+        },
+        body: jsonEncode(body),
+      );
+
+      // print(response.statusCode);
+      if(_successResponseCodes.contains(response.statusCode)) {
+        String data = response.body;
+        Map<String, dynamic> decodedData = jsonDecode(data);
+        return decodedData;
+      }else{
+        throw Exception(response.statusCode);
+      }
+    } catch (e) {
+      logger.e(e);
+      throw Exception(e);
+    }
+  }
+}
---
+++ lib/serviceUrls.dart
@@ -0,0 +1,6 @@
+library service_url;
+
+const String domain = 'http://192.168.1.102:5001/api';
+
+const String registerDriver = '$domain/auth/driver/register';
+const String loginDriver = '$domain/auth/driver/login';
